71f6569cb0068e6633170b4e2d1be9e689597c43
max-maslyuk <maxmas@list.ru>
2020-09-21 15:37:13 +0300

    Add files via upload

---
+++ 4_4.3_box ceremony.py
@@ -0,0 +1,7 @@
+boxw = int(input())
+if boxw <= 59:
+    print("Легкий вес")
+elif boxw >= 60 and boxw <= 63:
+    print("Первый полусредний вес")
+elif boxw >= 64 and boxw <=68:
+    print("Полусредний вес")
