fbcf01ef3eac2c8ca24152bb8edd4d46d9067051
max-maslyuk <maxmas@list.ru>
2020-10-01 17:52:26 +0300

    Add files via upload

---
+++ 6_6.2_srednie_znacheniya.py
@@ -0,0 +1,12 @@
+# Средние значения
+from math import *
+a = float(input())
+b = float(input())
+sred_arifmet = (a + b) / 2
+sred_geomet = sqrt(a * b)
+sred_garm = (2 * a * b) / (a + b)
+sred_kvadratich = sqrt((a ** 2 + b ** 2) / 2)
+print(sred_arifmet)
+print(sred_geomet)
+print(sred_garm)
+print(sred_kvadratich)
